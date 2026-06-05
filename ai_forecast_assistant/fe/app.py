80f75306310a60501debb3a431cb6ffde6bd592f
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-06-05 22:32:33 +0530

    weather tool and agent creation and llm and simple agentic ai project

---
+++ ai_forecast_assistant/fe/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import requests
+
+S_URL = "http://127.0.0.1:8000"
+
+st.title("🌤 AI Weather Agent")
+
+city = st.text_input(
+    "Enter City"
+)
+
+question = st.text_input(
+    "Ask Your Weather Question"
+)
+
+
+if st.button("Ask Agent"):
+    res=requests.post(f"{S_URL}/get_weather",params={
+        "city":city,
+        "question":question
+    })
+
+    st.success(res.json()["messages"][-1]["content"])
